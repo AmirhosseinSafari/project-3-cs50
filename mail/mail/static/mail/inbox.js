5840ad657d12874b582c2a4f76308b388688c8f8
Amirhossein <dollerbankas@gmail.com>
2020-12-03 11:04:33 -1200

    send mail part done

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
+  document.querySelector('#compose-form').onsubmit = send_mail;
 
   // By default, load the inbox
   load_mailbox('inbox');
@@ -30,4 +31,41 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
+
+function send_mail(){
+
+  const recipients = document.querySelector('#compose-recipients').value;
+  const subject = document.querySelector('#compose-subject').value;
+  const body = document.querySelector('#compose-body').value;
+
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+        recipients: recipients,
+        subject: subject,
+        body: body
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+      // Print result
+      console.log(result);
+      
+      if(result.message){
+      // Redirect to send
+        load_mailbox('sent');
+      }
+
+      if(result.error){
+      // Showing error msg
+        document.querySelector("#error-compose-form").innerHTML = `<p>${result.error}</p>`;
+      }
+
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+  return false
 }
--- mail/mail/views.py
+++ mail/mail/views.py
@@ -93,6 +93,7 @@ def mailbox(request, mailbox):
 
     # Return emails in reverse chronologial order
     emails = emails.order_by("-timestamp").all()
+
     return JsonResponse([email.serialize() for email in emails], safe=False)
 
 
